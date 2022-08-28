0671bfcf0258ba54d5587a8e6f2680f36ad900aa
TimIntim <timintim1995@gmail.com>
2022-08-28 15:44:51 +0500

    Add project files.

---
+++ DivideAndConquer/Calculator.cs
@@ -0,0 +1,61 @@
+﻿namespace DivideAndConquer
+{
+    public static class Calculator
+    {
+        public static int SumUsingLoop(List<int> nums)
+        {
+            int result = 0;
+            foreach (int i in nums)
+                result += i;
+
+            return result;
+        }
+
+        public static int SumUsingRecursion(IEnumerable<int> nums)
+        {
+            if (!nums.Any()) return 0;
+            if (nums.Count() == 1) return nums.First();
+
+            int item = nums.Last();
+            return item + SumUsingRecursion(nums.SkipLast(1));
+        }
+
+        public static int Count<T>(IEnumerable<T> list)
+        {
+            if (!list.Any()) return 0;
+
+            return 1 + Count(list.Skip(1));
+        }
+
+        public static int FindBiggest(IEnumerable<int> nums)
+        {
+            if (!nums.Any()) return 0;
+            if (nums.Count() == 1) return nums.First();
+
+            int firstInt = nums.First();
+            int nextInt = FindBiggest(nums.Skip(1));
+
+            return firstInt > nextInt ? firstInt : nextInt;
+        }
+
+        public static int BinarySearch(int[] sortedArr, int value, int? left = null, int? right = null)
+        {
+            left ??= 0;
+            right ??= sortedArr.Length - 1;
+            
+            if (left.Value > right.Value || !sortedArr.Any())
+                return -1;
+
+            int mid = (left.Value + right.Value) / 2;
+
+            if (sortedArr[mid] == value)
+                return mid;
+            else if (sortedArr[mid] < value)
+                left = mid + 1;
+            else
+                right = mid - 1;
+            
+            return BinarySearch(sortedArr, value, left, right);   
+        }
+    }
+}
---
+++ DivideAndConquer/Program.cs
@@ -0,0 +1,23 @@
+﻿using DivideAndConquer;
+
+List<int> nums = new List<int> () { 2, 4, 6};
+
+Console.WriteLine(Calculator.SumUsingLoop(nums));
+Console.WriteLine(Calculator.SumUsingRecursion(nums));
+
+Console.WriteLine(Calculator.SumUsingLoop(new List<int> { }));
+Console.WriteLine(Calculator.SumUsingRecursion(new int[] {}));
+
+Console.WriteLine(Calculator.Count(nums));
+int[] bigArray = new int[100];
+Console.WriteLine(Calculator.Count(bigArray));
+
+List<int> list = new List<int> () { 2, 3, 55, 64, 153, 154};
+Console.WriteLine(Calculator.FindBiggest(list));
+
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 55));
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 2));
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 153));
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 153));
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 1));
+Console.WriteLine(Calculator.BinarySearch(list.ToArray(), 155));
