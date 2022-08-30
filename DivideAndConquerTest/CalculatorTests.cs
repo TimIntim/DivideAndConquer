f35f0678c49737d1bd8a762fea13d2ff73e696d6
TimIntim <timintim1995@gmail.com>
2022-08-30 20:38:44 +0500

    начал писать тесты

--- DivideAndConquerTest/CalculatorTests.cs
+++ DivideAndConquerTest/CalculatorTests.cs
@@ -1,11 +1,26 @@
+using DivideAndConquer;
+
 namespace DivideAndConquerTest
 {
     public class CalculatorTests
     {
         [Fact]
-        public void Test_SumUsingLoop()
+        public void SumUsingLoop_EmptyArray_ReturnZero()
         {
+            var list = new List<int>();
+            var result = Calculator.SumUsingLoop(list);
+            Assert.Equal(0, result);
+        }
 
+        [Theory]
+        [InlineData(new[] { 1 }, 1)]
+        [InlineData(new[] { 1 , 2}, 3)]
+        [InlineData(new[] { 1, 2, 3 }, 6)]
+        public void SumUsingLoop_NumsArray_ReturnSumOfNums(int[] input, int expected)
+        {
+            var list = input.ToList();
+            var result = Calculator.SumUsingLoop(list);
+            Assert.Equal(expected, result);
         }
     }
 }
